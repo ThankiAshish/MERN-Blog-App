5d0e21b741dfd327730b80c09b2b7bc186ce816f
ThankiAshish <thankiashish555@gmail.com>
2024-01-04 14:06:33 +0530

    Redesigned Home Page

---
+++ client/src/screens/Home.jsx
@@ -0,0 +1,13 @@
+import Article from "./components/Article";
+import Header from "./components/Header";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Article />
+    </>
+  );
+};
+
+export default Home;
