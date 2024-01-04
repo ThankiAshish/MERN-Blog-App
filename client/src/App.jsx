a72a70a95159eb683b2aca85761957237333c696
ThankiAshish <thankiashish555@gmail.com>
2024-01-04 14:06:22 +0530

    Added Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
-import Header from "./screens/components/Header";
-import Article from "./screens/components/Article";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import Register from "./screens/Register";
 
 const App = () => {
   return (
-    <>
-      <Header />
-      <Article />
-      <Article />
-      <Article />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 };
 
