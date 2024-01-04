cf3d8f7d5efd8bd52243123277334bfc6c986789
ThankiAshish <thankiashish555@gmail.com>
2024-01-04 14:06:41 +0530

    Added Login Page

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -1,5 +1,43 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <section className="auth-section">
+      <form>
+        <h1>Login</h1>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          placeholder="Enter Username"
+          className="input-field"
+        />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Enter Password"
+          className="input-field"
+        />
+        <button type="submit" className="btn btn-full">
+          Login
+        </button>
+
+        <p>
+          Forgot Password?&nbsp;
+          <Link to="/reset" className="link">
+            Reset
+          </Link>
+        </p>
+        <p>
+          Don&apos;t Have an Account?&nbsp;
+          <Link to="/register" className="link">
+            Register
+          </Link>
+        </p>
+      </form>
+    </section>
+  );
 };
 
 export default Login;
