5dc9398595810fb9d515ce613e7a7c170efbc814
ThankiAshish <thankiashish555@gmail.com>
2024-01-05 09:23:42 +0530

    Added Mongoose Connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,13 @@
 const mongoose = require("mongoose");
+
+const connection = mongoose.connect(process.env.MONGODB_URL);
+
+connection
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((error) => {
+    console.log("MongoDB connection error:", error);
+  });
+
+module.exports = connection;
