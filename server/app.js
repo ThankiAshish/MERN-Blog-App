2859e67274a60947d14913acd10e2b41feb8633c
ThankiAshish <thankiashish555@gmail.com>
2024-01-05 09:23:26 +0530

    Created Server

--- server/app.js
+++ server/app.js
@@ -1,9 +1,26 @@
-require("dotenv").config("../.env");
+require("dotenv").config({ path: "../.env" });
 
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+
+require("./config/db");
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/uploads", express.static("uploads"));
+
+app.use("/api/auth", require("./routes/authRoute"));
+app.use("/articles", require("./routes/articleRoute"));
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
