3386a9e0c6a60948dae178d285a492bd845f60e8
ThankiAshish <thankiashish555@gmail.com>
2024-01-05 11:31:16 +0530

    Added Proxy

--- server/app.js
+++ server/app.js
@@ -12,14 +12,22 @@ require("./config/db");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/uploads", express.static("uploads"));
 
+app.use("/", (req, res, next) => {
+  const token = req.cookies.token;
+  if (token) {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
+  }
+  next();
+});
 app.use("/api/auth", require("./routes/authRoute"));
-app.use("/", require("./routes/articleRoute"));
+app.use("/api", require("./routes/articleRoute"));
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
