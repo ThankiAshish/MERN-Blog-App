f9e5bcd4885af76c073220a9ef7c25fa2db2f090
Ashish <thankiashish555@gmail.com>
2024-01-06 22:03:05 +0530

    Added UserContext

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,29 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import UserProvider from "./context/UserContext";
+
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import Register from "./screens/Register";
 import Reset from "./screens/Reset";
+import CreateArticle from "./screens/CreateArticle";
+import Layout from "./layouts/Layout";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/reset" element={<Reset />} />
-      </Routes>
-    </Router>
+    <UserProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/create-article" element={<CreateArticle />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/reset" element={<Reset />} />
+        </Routes>
+      </Router>
+    </UserProvider>
   );
 };
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import App from "./App.jsx";
@@ -12,14 +11,6 @@ import "./assets/styles/queries.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <div className="container">
-      <ToastContainer
-        position="top-center"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick={true}
-        theme="dark"
-      />
       <App />
     </div>
   </React.StrictMode>
