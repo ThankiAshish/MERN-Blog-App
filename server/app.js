a6fb0bb3f5e2e00b1e0c5ddf5534f4c3a83250c6
Ashish <thankiashish555@gmail.com>
2024-01-07 16:55:54 +0530

    Removed Unncessary Code

--- server/app.js
+++ server/app.js
@@ -3,8 +3,6 @@ require("dotenv").config({ path: ".env" });
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const jwt = require("jsonwebtoken");
-const multer = require("multer");
 const path = require("path");
 
 require("./config/db");
@@ -16,10 +14,9 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/uploads", express.static("uploads"));
 
 app.use("/api/auth", require("./routes/authRoute"));
-app.use("/api", require("./routes/articleRoute"));
+app.use("/api/article", require("./routes/articleRoute"));
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
