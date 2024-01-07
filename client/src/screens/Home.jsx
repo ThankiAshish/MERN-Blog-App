61a03c0a8a913b793b547b9de98b4aa82da9fd00
Ashish <thankiashish555@gmail.com>
2024-01-07 16:54:13 +0530

    Made Home Page Dynamic

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,9 +1,47 @@
-import Article from "./components/Article";
+import { useState, useEffect } from "react";
+import { toast } from "react-toastify";
+
+import Article from "./components/ArticleModel";
 
 const Home = () => {
+  const [articles, setArticles] = useState([]);
+
+  const getArticles = async () => {
+    try {
+      const response = await fetch(`api/article`);
+
+      if (response.status === 200) {
+        const data = await response.json();
+        setArticles(data);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getArticles();
+  }, []);
+
   return (
     <>
-      <Article />
+      {articles.length > 0 ? (
+        articles.map((article) => (
+          <Article
+            key={article._id}
+            id={article._id}
+            cover={article.cover}
+            title={article.title}
+            summary={article.summary}
+            author={article.author}
+            date={article.updatedAt}
+          />
+        ))
+      ) : (
+        <div className="default-page">
+          <h1>No articles found!</h1>
+        </div>
+      )}
     </>
   );
 };
